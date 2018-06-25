89b18aac5b16804340c6c4ce5fe693474fb8cae4
Mati <matias@cumplo.com>
2018-06-25 18:12:08 -0400

    segmentation fault

--- Cell.cpp
+++ Cell.cpp
@@ -5,35 +5,55 @@ class Cell
 {
 	public:
 		bool state;
-		int x_pos, y_pos;
-		sf::RectangleShape* pointer;
+		float x_pos, y_pos;
+		sf::RectangleShape* rectangle;
 
 		Cell() {}
-		
-		Cell(bool state, int x_pos, int y_pos, sf::RectangleShape* pointer)
+
+		Cell(float x_pos, float y_pos)
 		{
-			this -> state   = state;
-			this -> x_pos   = x_pos;
-			this -> y_pos   = y_pos;
-			this -> pointer = pointer;
+			sf::RectangleShape rectangle;
+			rectangle.setPosition(x_pos, y_pos);
+			rectangle.setOutlineColor(sf::Color::Black);
+
+			this -> state     = false;
+			this -> x_pos     = x_pos;
+			this -> y_pos     = y_pos;
+			this -> rectangle = &rectangle;
+		}
+		
+		Cell(bool state, float x_pos, float y_pos, sf::RectangleShape* rectangle)
+		{
+			this -> state     = state;
+			this -> x_pos     = x_pos;
+			this -> y_pos     = y_pos;
+			this -> rectangle = rectangle;
 		}
 
-		void setState(bool alive) {
-			state = alive;
+		void setState(bool state)
+		{
+			this -> state = state;
 		}
 
-		sf::RectangleShape* getRectanglePointer() {
-			return pointer;
+		void awake()
+		{
+			this -> setState(true);
+			this -> rectangle -> setOutlineThickness(10000);
+			this -> rectangle -> setOutlineColor(sf::Color::White);
+		}
+
+		void sleep()
+		{
+			this -> setState(false);
+			this -> rectangle -> setOutlineThickness(0);
+			this -> rectangle -> setOutlineColor(sf::Color::Black);
 		}
 };
 
-//int main(int argc, char const *argv[])
-//{
-//	sf::RectangleShape rectangle;
-//	rectangle.setOutlineThickness(10.0);
-//	
-//	Cell celulitica(true, 2, 3, &rectangle);
-//
-//	std::cout << celulitica.pointer -> getOutlineThickness() << std::endl;	
-//	return 0;
-//}
+int main(int argc, char const *argv[])
+{
+	Cell cell(2.0, 4.0);
+	cell.awake();
+	std::cout << cell.rectangle -> getOutlineThickness() << std::endl;	
+	return 0;
+}
