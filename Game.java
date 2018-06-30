3c0c6df5d7a8bcae023977fd51cffc3ab3e32c47
Mati <matipacheco@gmail.com>
2018-06-30 11:02:55 -0400

    trying to make shit work

--- Game.java
+++ Game.java
@@ -1,4 +1,12 @@
+import java.awt.Graphics;
+
 class Game
 {
-	
+	public static void main(String[] args) {
+		int size = 10;
+		Cluster cluster = new Cluster(size);
+		System.out.println(cluster.getCell(2, 2));
+		Graphics g;
+		cluster.initialize(g);
+	}
 }
