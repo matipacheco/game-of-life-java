c7f54ec2f60f29ba4a6eab2bfbfbd0c88c2eef67
Mati <matipacheco@gmail.com>
2018-06-30 08:18:41 -0400

    when it was ment to be a C++ app

--- Cluster.cpp
+++ Cluster.cpp
@@ -1,22 +1,43 @@
+#include <iostream>
 #include "Cell.cpp"
 
 class Cluster
 {
 	public:
 		int size;
-		Cell** matrix;
+		int** matrix;
 
 		Cluster(int size)
 		{
-			Cell matrix[size][size];
+			int** matrix = new int* [size];
 
+			for(int i = 0; i < size; ++i)
+				matrix[i] = new int [size];
+			
 			this -> size   = size;
 			this -> matrix = matrix;
 		}
+
+		int getSize()
+		{
+			return this -> size;
+		}
+
+		int getCell(int row, int column)
+		{
+			return this -> matrix[row][column];
+		}
 };
 
-int main(int argc, char const *argv[])
-{
-	Cluster cluster(6);
-	return 0;
-}
+// int main(int argc, char const *argv[])
+// {
+// 	Cluster cluster(6);
+// 	for (int rows = 0; rows < cluster.getSize(); ++rows)
+// 	{
+// 		for (int cols = 0; cols < cluster.getSize(); ++cols)
+// 		{
+// 			std::cout << cluster << std::endl;
+// 		}
+// 	}
+// 	return 0;
+// }
---
+++ Game.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char const *argv[])
+{
+	// instanciar Cluster(dimension_matriz)
+	// llenar el Cluster de dimension_matriz**2 instancias de Cell
+	// Cell es una clase que tiene un state (vivo o muerto) y un puntero a un rectangulo
+	// el color del rectangulo cambia dependiendo de si la celula está viva o muerta
+	return 0;
+}
